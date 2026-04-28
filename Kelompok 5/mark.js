e4bf8bd35b9dab9aa93b132ec749bb96f100042b
r1an <163669265+rynhdsmdr@users.noreply.github.com>
2026-04-28 23:13:19 +0700

    feat: tambah lokasi Universitas Terbuka, Mie Ayam Abu Khalil dan Alfamart Lambung

--- Kelompok 5/mark.js
+++ Kelompok 5/mark.js
@@ -5,4 +5,26 @@ const markers = [
     popup: "Kos Hizqil",
     owner: "hizqil",
   },
+
+  {
+    lat: 5.550292,
+    lng: 95.304013,
+    popup: "Universitas Terbuka",
+    owner: "riyan",
+  },
+
+   {
+    lat: 5.553685,
+    lng: 95.296468,
+    popup: "Mie Ayam Abu Khalil",
+    owner: "riyan",
+  },
+
+   {
+    lat: 5.553563,
+    lng: 95.294603,
+    popup: "Alfamart Lambung",
+    owner: "riyan",
+  }
+  
 ];
